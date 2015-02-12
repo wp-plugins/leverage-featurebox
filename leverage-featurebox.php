de12c164d8a65413ae0bb32767e139c32638d8d8
zlsmith <zlsmith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-12 17:24:25 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/leverage-featurebox/trunk@1088284 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ leverage-featurebox.php
@@ -0,0 +1,81 @@
+<?php
+/*
+* Plugin Name: Leverage FeatureBox
+* Plugin URI: http://www.increasingleverage.com
+* Description: The Leverage FeatureBox widget converts website visitors into email subscribers.
+* Version: 1.0
+* Author: Zachary Smith | Increasing Leverage
+* Author URI: http://www.increasingleverage.com
+* Copyright 2015 Zachary Smith
+*/
+class leverage_featurebox extends WP_Widget {
+public function __construct() {
+		parent::__construct(
+	 		'leverage_featurebox', // Base ID
+			'Leverage FeatureBox', // Name
+ 
+			array( 'description' => __( 'The Leverage FeatureBox widget converts website visitors into subscribers', 'zs' ), ) // Args
+		);
+	}
+public function widget( $args, $instance )
+	{
+		extract( $args );
+		$input = $before_widget;
+		static $i = 0;
+		$i++;
+
+		$input .= '<div class="widget_' . $i . '">
+
+		<h2>' . $instance['title'] . '</h2>';
+		$input .= '<img src="' . $instance[ 'image_uri'] . '" class="alignright">';
+		$input .= '<p>' . $instance['copy'] . '</p>';
+		$input .= '</br>';
+		$input .= $instance[ 'form' ];
+		$input .= '</div>';
+		$input .= $after_widget;
+		echo apply_filters( 'widget_leverage_feature_box', $input );
+	}
+	public function update( $new_instance, $old_instance )  
+	{
+		$instance = $old_instance;
+		foreach( $new_instance as $key=>$val )
+		{
+			$instance[ $key ] = $val;
+		}
+		return $instance;
+	}
+	public function form( $instance )
+	{
+	    if ( isset( $instance[ 'image_uri' ] ) ) {
+			$image_uri = $instance[ 'image_uri' ];
+		}
+		else {
+			$image_uri = __( '', 'zs' );
+		}
+		$defaults = array(
+			'title' => '',
+			'copy' => '',
+			'form' => '',
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults);
+		?>
+<p>Title: <input class="widefat" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" /></p>
+		<p>Copy: <textarea rows="8" cols="20" class="widefat" name="<?php echo $this->get_field_name( 'copy' ); ?>"><?php echo esc_attr( $instance['copy'] ); ?></textarea></p>
+		<p>HTML form: <textarea rows="8" cols="20" class="widefat" name="<?php echo $this->get_field_name( 'form' ); ?>"><?php echo esc_attr( $instance['form'] ); ?></textarea></p>
+    <p>
+      <label for="<?php echo $this->get_field_id('image_uri'); ?>">Image</label><br />
+        <img class="custom_media_image" src="<?php echo $image_uri; ?>" style="margin:0;padding:0;max-width:100px;float:left;display:inline-block" />
+        <input type="text" class="widefat custom_media_url" name="<?php echo $this->get_field_name('image_uri'); ?>" id="<?php echo $this->get_field_id('image_uri'); ?>" value="<?php echo $image_uri; ?>">
+       </p>
+       <p>
+        <input type="button" value="<?php _e( 'Upload Image', 'zs' ); ?>" class="button custom_media_upload" id="custom_image_uploader"/>
+    </p>
+		<?php 
+	}
+}
+add_action( 'widgets_init', create_function( '', 'register_widget( "leverage_featurebox" );' ) );
+function leverage_script(){
+  wp_enqueue_media();
+  wp_enqueue_script('adsScript', plugins_url( '/js/leveragebox.js' , __FILE__ ));
+}
+add_action('admin_enqueue_scripts', 'leverage_script');
---
+++ readme.txt
@@ -0,0 +1,13 @@
+Leverage FeatureBox
+Contributors: Zachary_Smith
+Tags: featurebox, feature, box, image, widget, email subscription, leverage, form, before post, after post
+Requires at least: 3.5
+Tested up to: 4.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The Leverage FeatureBox widget enables you to quickly and easily create a quick call to action to convert visitors into email subscribers.
+
+== Description ==
+
+This responsive widget perfectly adjusts to any widgetized area of your website that you choose: feature area, sidebar, footer, and before or after-post areas.  The separate input areas for title, copy, and auto-responder code outputs on separate, double-spaced lines so you can quickly create a call to action without worrying about formatting.
